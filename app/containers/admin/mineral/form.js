92883215e6b047d6cfa204d19975f49c3a46b9ae
fahimezarghani@gmail.com <fahimezarghani@gmail.com>
2019-04-18 00:24:38 +0430

    Mineral Form

--- app/containers/admin/mineral/form.js
+++ app/containers/admin/mineral/form.js
@@ -2,53 +2,17 @@ import React, { Component } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import _ from 'underscore';
-import ReactSelect from 'react-select';
 
 const { Control, Group, Label } = Form;
 
 type Props = {
-  name: string,
   errors: object,
   values: object,
-  handleChange: () => void
+  touched: object,
+  handleChange: () => void,
+  handleSubmit: () => void,
+  formType: string
 };
-
-const AbundanceOptions = [
-  { value: 'Abundance1', label: 'Abundance1' },
-  { value: 'Abundance2', label: 'Abundance2' }
-];
-
-const TextInput = ({ name, values, errors, handleChange }: Props) => (
-  <Control
-    type="text"
-    name={name}
-    value={!_.isUndefined(values[name]) ? values[name] : ''}
-    onChange={handleChange}
-    isInvalid={!_.isUndefined(values[name]) && errors[name]}
-  />
-);
-
-const ErrorMessage = ({ errors, name, values }) => (
-  <>
-    {!_.isUndefined(values[name]) && (
-      <Control.Feedback type="invalid">{errors[name] || null}</Control.Feedback>
-    )}
-  </>
-);
-
-const TextField = ({ name, label, ...rest }) => (
-  <Group as={Row} controlId={name}>
-    <Label column sm={3}>
-      {label}
-    </Label>
-
-    <Col sm={9}>
-      <TextInput name={name} {...rest} />
-    </Col>
-
-    <ErrorMessage name={name} {...rest} />
-  </Group>
-);
 
 class ElementForm extends Component<Props> {
   render() {
@@ -70,10 +34,6 @@ class ElementForm extends Component<Props> {
 
     return (
       <Form noValidate onSubmit={handleSubmit} className="smfp-form-container">
-        {formType === 'register' && (
-          <TextField name="element" label="Element" {...this.props} />
-        )}
-
         <Group as={Row} controlId="Mineral_Title">
           <Label column sm={3}>
             Mineral Title
@@ -110,13 +70,17 @@ class ElementForm extends Component<Props> {
             Abundance
           </Label>
           <Col sm={9}>
-            <ReactSelect
-              value={values.abundance}
+            <Control
+              as="select"
               onChange={handleChange}
-              options={AbundanceOptions}
-              placeholder="Abundance"
               isValid={touched.abundance && !errors.abundance}
-            />
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Control>
           </Col>
         </Group>
 
@@ -140,13 +104,17 @@ class ElementForm extends Component<Props> {
             Elements
           </Label>
           <Col sm={9}>
-            <ReactSelect
-              value={values.elements}
+            <Control
+              as="select"
               onChange={handleChange}
-              options={AbundanceOptions}
-              placeholder="Elements"
               isValid={touched.elements && !errors.elements}
-            />
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Control>
           </Col>
         </Group>
 
