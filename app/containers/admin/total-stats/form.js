9d4d1ed581efd61711ff4c702f8ad705abb54bbf
parsaatef@gmail.com <parsaatef@gmail.com>
2019-04-21 16:29:59 +0430

    Complete Total Stats

--- app/containers/admin/total-stats/form.js
+++ app/containers/admin/total-stats/form.js
@@ -1,113 +1,48 @@
-import React, { Component } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import _ from 'underscore';
-
-const { Control, Group, Label } = Form;
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import AppForm from '../../../components/form/AppForm';
+import Field from '../../../components/form/Field';
 
 type Props = {
   formType: string,
-  errors: object,
-  values: object,
-  touched: object,
-  handleChange: () => void,
-  handleSubmit: () => void
+  initialValues: object,
+  validationSchema: object,
+  onSubmit: () => void
 };
 
-class ElementForm extends Component<Props> {
-  render() {
-    const {
-      handleSubmit,
-      handleChange,
-      /*
-        handleBlur,
-        */
-      values,
-      touched,
-      /*
-        isValid,
-        */
-      errors,
-      formType
-    } = this.props;
-    console.log('------this.props----', this.props);
+const TotalStatsForm = (props: Props) => {
+  const { onSubmit, formType, initialValues, validationSchema } = props;
 
-    return (
-      <Form noValidate onSubmit={handleSubmit} className="smfp-form-container">
-        <Group as={Row} controlId="total_stats_element">
-          <Label column sm={3}>
-            Element
-          </Label>
-          <Col sm={9}>
-            <Control
-              as="select"
-              value={values.element}
-              onChange={handleChange}
-              isValid={touched.element && !errors.element}
-            >
-              <option value="element1">Element 1</option>
-              <option value="element2">Element 2</option>
-            </Control>
-          </Col>
-          <Control.Feedback type="invalid">{errors.element}</Control.Feedback>
-        </Group>
+  return (
+    <AppForm
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      schema={validationSchema}
+    >
+      <Field
+        type="select"
+        name="element"
+        label="Element"
+        options={[
+          { value: 'element1', label: 'Element 1' },
+          { value: 'element2', label: 'Element 2' }
+        ]}
+        placeholder="Select Element"
+      />
 
-        <Group as={Row} controlId="total_stats_name">
-          <Label column sm={3}>
-            Name
-          </Label>
-          <Col sm={9}>
-            <Control
-              type="text"
-              name="name"
-              value={values.name}
-              onChange={handleChange}
-              isInvalid={!_.isUndefined(values.name) && errors.name}
-            />
-          </Col>
-          <Control.Feedback type="invalid">{errors.name}</Control.Feedback>
-        </Group>
+      <Field type="text" name="name" label="Name" />
 
-        <Group as={Row} controlId="total_stats_year">
-          <Label column sm={3}>
-            Year
-          </Label>
-          <Col sm={9}>
-            <Control
-              type="number"
-              name="year"
-              value={values.year}
-              onChange={handleChange}
-              isInvalid={!_.isUndefined(values.year) && errors.year}
-            />
-          </Col>
-        </Group>
+      <Field type="number" name="year" label="Year" />
 
-        <Group as={Row} controlId="total_stats_value">
-          <Label column sm={3}>
-            Value
-          </Label>
-          <Col sm={9}>
-            <Control
-              type="text"
-              name="value"
-              value={values.value}
-              onChange={handleChange}
-              isInvalid={!_.isUndefined(values.value) && errors.value}
-            />
-          </Col>
-        </Group>
+      <Field type="number" name="value" label="Value" />
 
-        <Button type="submit">
-          {formType === 'register' ? <>Add New Element</> : <>Update Element</>}
-        </Button>
-      </Form>
-    );
-  }
-}
+      <Field type="text" name="unit" label="Unit" />
 
-const mapStateToProps = state => ({
-  location: state.router.location
-});
+      <Button type="submit">
+        {formType === 'register' ? <>Add New Stat</> : <>Update Stat</>}
+      </Button>
+    </AppForm>
+  );
+};
 
-export default connect(mapStateToProps)(ElementForm);
+export default TotalStatsForm;
