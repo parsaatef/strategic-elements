b29bcf436370af7c6519ab394ff5ebb27c4784fe
fahimezarghani@gmail.com <fahimezarghani@gmail.com>
2019-05-27 18:05:38 +0430

    heading

---
+++ app/components/General/PageHeadingIcon.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class PageHeadingIcon extends Component<Props> {
+  render() {
+    const { className, title, icon } = this.props;
+
+    return (
+      <div
+        className={`page-heading-icon animated bounceInDown slow ${className}`}
+      >
+        <h4 className="page-heading-icon-title">
+          <span className={`page-heading-icon-icon ${icon}`} />
+          {title}
+        </h4>
+      </div>
+    );
+  }
+}
